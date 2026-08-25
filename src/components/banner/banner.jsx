b1c569699e5f6265c653d2ba421397be22cd0cbb
isabelyrochaoliveira <isabelyrocha.dev@gmail.com>
2026-08-25 11:21:03 -0300

    feat: implementa componente banner com logo

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,11 @@
+import './banner.css';
+
+const Banner = () => {
+  return (
+    <header className="banner">
+      <img src="/imagens/logo.png" alt="Nexus Tech - Artigos de Informática" />
+    </header>
+  );
+};
+
+export default Banner;
